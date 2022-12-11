282148c69644a7923e4d6e94d1de45b35eb084c5
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-12-11 01:19:57 -0500

    added Global.modal.css to styles folder

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,13 @@
-import itemsList from './Items'
-import styles from '../styles/Display.css'
+// import itemsList from './Items'
+// import styles from '../styles/Display.css'
+import styles from '../styles/Global.module.css'
 
 
 const Display = () => {
     return (
-        <div className={styles["display-container"]}>
-            <h7 className={styles["display-header"]}>Today's Selection</h7>
-            <div className={styles["display-section"]}>
+        <div className={styles['display-container']}>
+            <span className={styles['display-header']}>Today's Selection</span>
+            <div className={styles['display-section']}>
 
             </div>
         </div>
