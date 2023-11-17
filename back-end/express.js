de9cafaf3f6f4102ea79c06723417300052376de
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2023-11-17 17:49:30 +0000

    Update 3 files

--- back-end/express.js
+++ back-end/express.js
@@ -1,9 +1,29 @@
+// requirements
+const axios = require("axios");
 const express = require("express");
+// Common JS Module Compatibility
+// const fetch = require("node-fetch");
+// general variables
 const [app, port] = [express(), 5000];
 
+// fetch?
+// fetch("https://api.nyc.gov/public/api")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+import("node-fetch")
+  .then(({ default: fetch ("https://api.nyc.gov/public/api/") }) => {
+    // Use 'fetch' here after it's been imported
+    // Rest of your code that uses 'fetch'
+  })
+  .catch((err) => {
+    // Handle any potential import errors here
+    console.error("Error importing 'node-fetch':", err);
+  });
+
 // what are we getting
-app.get(`/`, function (req, res) {
-  res.send(`Hello world!`);
+app.get(`/`, async function (req, res) {
+  await res.send(data);
 });
 
 // are we listening?
