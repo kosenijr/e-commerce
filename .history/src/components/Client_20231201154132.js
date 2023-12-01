40ea031124e6df9a02c00b820c791ea33d6854dc
Kayode Oseni <k.oseni.jr@gmail.com>
2023-12-01 16:12:24 -0500

    fixing files

---
+++ .history/src/components/Client_20231201154132.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const log = console.log;
+
+// get useEffect for fetch api
+log(process.env.URL_311);
+
+const Client = () => {
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    const fetchData = fetch("https://ttzswd-5000.csb.app")
+      .then((res) => res.json())
+      .then((data) => log(data))
+      .catch((err) => console.error(`Error: ${err}`));
+  }, []);
+
+  return <p>Hey</p>;
+};
+
+export default Client;
