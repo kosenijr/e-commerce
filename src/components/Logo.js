adb5c23be26d4fd43c7c0b54ea2ee446224e95b7
Kayode Oseni <k.oseni.jr@gmail.com>
2023-12-11 04:44:35 -0500

    cleared git lfs and reinstated actual code

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a7d903de6850c84a890d0cdaa8671189c38809a9ccd7c396050faf93c532c752
-size 297
+import styles from '../styles/Logo.module.css'
+import img from '../tf_logo/TicketFree.jpg'
+
+
+
+const Logo = () => {
+    return (
+        <div id={styles['asp-logo']}>
+            <img className={styles['logo-img']} src={img} alt='logo'/>
+        </div>
+    )
+
+
+
+}
+export default Logo
