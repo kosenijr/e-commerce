cec9ecd351ab95a178f4368f6d5f6016f9492e73
Kayode Oseni <k.oseni.jr@gmail.com>
2023-12-18 06:55:00 -0500

    will need to fix form formatting before and after errors, set up form validation, and return to backend

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import styles from "../styles/FormInput.module.css";
 
 const FormInput = (props) => {
+  const [error, setError] = useState(false);
+
+  const handleInputChange = (e) => {
+    setError(!props.validate(e.target.value));
+  }
+
   return (
     <div className={styles["formInput"]}>
       <section className={styles["section-1"]}>
         <label>{props.name}</label>
+        <div className={styles["input-wrapper"]}>
         <input placeholder={props.placeholder} />
+        </div>
       </section>
       <section className={styles["section-2"]}>
-        <p>Incorrect input. Please enter a proper {props.value}.</p>
+        {error && <p>Incorrect input. Please enter a proper {props.value}.</p>}
       </section>
     </div>
   );
