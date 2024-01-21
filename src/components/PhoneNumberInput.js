113a6e621b509525807c05f65287b4f1f590b888
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2024-01-21 00:43:00 +0000

    Update PhoneNumberInput.js

--- src/components/PhoneNumberInput.js
+++ src/components/PhoneNumberInput.js
@@ -12,37 +12,49 @@ const PhoneNumberInput = (props) => {
 
     const formattedNumber = numericInput.replace(
       /^1?(\d{3})(\d{3})(\d{4})$/,
-      "($1) $2-$3"
+      "($1) $2-$3",
     );
     return formattedNumber;
   };
 
   const handleNumberChange = (e) => {
     const input = e.target.value;
-    const formattedNumber = formatPhoneNumber(input);
-    //  update with formatted number
-    setPhoneNumber(formattedNumber);
-    
+
+    // regular expressions
+    const addOne = /^1\d{10}$/;
+    const doNotAddOne = /^\d{10}$/;
+
+    // check conditions and update state
+    if (input.startsWith("1") && addOne.test(input)) {
+      setPhoneNumber(input.slice(0, 11));
+    } else if (!input.startsWith("1") && doNotAddOne.test(input)) {
+      setPhoneNumber(input.slice(0, 10));
+
+      const formattedNumber = formatPhoneNumber(input);
+      //  update with formatted number
+      setPhoneNumber(formattedNumber);
+    }
+
+    return (
+      <div>
+        <section className={styles["section-1"]}>
+          <label>Phone Number</label>
+          <input
+            name="Phone Number"
+            onChange={handleNumberChange}
+            placeholder="(123) 456-7890"
+            value={phoneNumber}
+          />
+        </section>
+        <section className={styles["section-2"]}>
+          {errors && (
+            <p>
+              Incorrect input. Please enter your {props.name.toLowerCase()}.
+            </p>
+          )}
+        </section>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <section className={styles["section-1"]}>
-        <label>Phone Number</label>
-        <input
-          name="Phone Number"
-          onChange={handleNumberChange}
-          placeholder="(123) 456-7890"
-          value={phoneNumber}
-        />
-      </section>
-      <section className={styles["section-2"]}>
-        {errors && (
-          <p>Incorrect input. Please enter your {props.name.toLowerCase()}.</p>
-        )}
-      </section>
-    </div>
-  );
 };
-
 export default PhoneNumberInput;
