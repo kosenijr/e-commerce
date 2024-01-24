5b7d4d9e4fa749288bcdd15d0a76f42292c990f5
Kayode Oseni <k.oseni.jr@gmail.com>
2024-01-24 02:07:15 -0500

    form error matters completed

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -16,17 +16,14 @@ const FormInput = (props) => {
       case "City":
         setErrors(!/^[A-Za-z\s-]+$/.test(inputValue));
         break;
-      case "Address Line 1":
-        setErrors(!/^\d{1,5}[-\s]?\w*\.?\s[A-Za-z\s0-9#-]+$/.test(inputValue));
-        break;
       case "Address Line 2":
         setErrors(!/^[A-Za-z0-9\s#-.]*$/.test(inputValue));
         break;
-      case "Email":
-        setErrors(
-          !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(inputValue)
-        );
-        break;
+      // case "Email":
+      //   setErrors(
+      //     !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(inputValue)
+      //   );
+      //   break;
       case "State":
         setErrors(!/^[A-Za-z]{2}$/.test(inputValue));
         break;
@@ -38,9 +35,28 @@ const FormInput = (props) => {
         break;
     }
   };
-
-  return (
-    <div className={styles["formInput"]} onChange={handleInputChange}>
+  
+  // on blur handling
+  const handleInputBlur = (e) => {
+        switch (props.name) {
+          case "Address Line 1":
+            setErrors(!/^\d{1,5}[-\s]?\w*\.?\s[A-Za-z0-9\s#-]+$/i.test(e.target.value));
+            break;
+            case "Email":
+              setErrors(!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value));
+              break;
+              default:
+                setErrors(false); // No validation by default
+                break;
+                            
+          }
+        };
+        
+        return (
+    <div
+      className={styles["formInput"]}
+      onChange={handleInputChange}
+    >
       <section className={styles["section-1"]}>
         <label>{props.name}</label>
         <input
@@ -48,6 +64,7 @@ const FormInput = (props) => {
           placeholder={props.placeholder}
           onChange={handleValueChange}
           maxLength={props.name === "Zip Code" ? 5 : undefined}
+          onBlur={handleInputBlur}
         />
       </section>
       <section className={styles["section-2"]}>
